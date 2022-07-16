8b33590eaa8344f222bbd1c65368a65656a0f61f
Luke Tyler <53444096+lukejamestyler@users.noreply.github.com>
2022-07-16 17:57:18 -0400

    first commit

--- src/pages/NotesListPages.js
+++ src/pages/NotesListPages.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import ListItem from '../components/ListItem'
-import AddButton from '../components/AddButton'
+import AddButton from '../components/AddButton.js'
 
 const NotesListPages = () => {
 
